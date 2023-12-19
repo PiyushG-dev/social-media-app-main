a1a1c59c99542a75fa250c6238109ce483ca26fb
PiyushG-dev <pjgpiyush@gmail.com>
2023-12-19 19:49:01 +0530

    search bar added

---
+++ src/components/subscription/Subscription.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Subscription.module.css";
+
+const Subscription = () => {
+  return <div className={styles.container}></div>;
+};
+
+export default Subscription;
--- src/components/trending/Trending.jsx
+++ src/components/trending/Trending.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import styles from "./Trending.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const Trending = () => {
-  return <div className={styles.container}>trending</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.search_container}>
+        <FontAwesomeIcon className={styles.search} icon={faSearch} />
+        <input type="text" placeholder="Search" />
+      </div>
+    </div>
+  );
 };
 
 export default Trending;
