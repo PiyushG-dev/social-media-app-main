a2417202fdf79787b54b61179396004e9833fbd9
PiyushG-dev <pjgpiyush@gmail.com>
2024-01-11 13:54:28 +0530

    createPost component added

--- src/components/createPost/CreatePost.jsx
+++ src/components/createPost/CreatePost.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import styles from "./CreatePost.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -12,8 +13,44 @@ import {
   faLocationArrow,
 } from "@fortawesome/free-solid-svg-icons";
 import prof from "../../images/pf2.jpg";
+import { useDispatch } from "react-redux";
+import { addPost } from "../../features/post/postSlice";
 
 const CreatePost = ({ createPost, setCreatePost }) => {
+  const [postDesc, setPostDesc] = useState("");
+  const [selectedImage, setSelectedImage] = useState(null);
+  const dispatch = useDispatch();
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedImage(file);
+
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      const imageDataUrl = reader.result;
+      console.log(imageDataUrl);
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+      console.log(file);
+    }
+  };
+
+  const handlePost = () => {
+    if ((postDesc && selectedImage) || postDesc) {
+      dispatch(
+        addPost({ description: postDesc, selectedImage: selectedImage })
+      );
+    } else {
+      alert("description missing");
+    }
+
+    setPostDesc("");
+    setSelectedImage(null);
+    setCreatePost(!createPost);
+  };
   return (
     <section
       style={{ display: !createPost ? "none" : "flex" }}
@@ -30,7 +67,13 @@ const CreatePost = ({ createPost, setCreatePost }) => {
         </div>
         <div className={styles.tweet_content_container}>
           <img className={styles.profile_img} src={prof} alt="profile" />
-          <textarea rows="4" cols="40" placeholder="What is happening?!" />
+          <textarea
+            rows="4"
+            cols="40"
+            placeholder="What is happening?!"
+            value={postDesc}
+            onChange={(e) => setPostDesc(e.target.value)}
+          />
         </div>
         <div className={styles.reply_option_container}>
           <FontAwesomeIcon className={styles.earth} icon={faEarth} />
@@ -43,7 +86,12 @@ const CreatePost = ({ createPost, setCreatePost }) => {
                 style={{ cursor: "pointer" }}
                 icon={faPhotoFilm}
               />
-              <input type="file" accept="image/*" style={{ display: "none" }} />
+              <input
+                type="file"
+                accept="image/*"
+                style={{ display: "none" }}
+                onChange={handleImageChange}
+              />
             </label>
             <FontAwesomeIcon icon={faSquareCheck} />
             <FontAwesomeIcon icon={faList} />
@@ -51,7 +99,9 @@ const CreatePost = ({ createPost, setCreatePost }) => {
             <FontAwesomeIcon icon={faCalendarAlt} />
             <FontAwesomeIcon icon={faLocationArrow} />
           </div>
-          <button className={styles.post_button}>Post</button>
+          <button onClick={handlePost} className={styles.post_button}>
+            Post
+          </button>
         </div>
       </div>
     </section>
