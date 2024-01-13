f83594bb0b3c4c3fbf46908e4f218495d539beda
PiyushG-dev <pjgpiyush@gmail.com>
2024-01-13 11:47:49 +0530

    loginUser notification added

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -2,6 +2,8 @@ import React, { createContext, useEffect, useState } from "react";
 import { account } from "../appwrite/auth";
 import { useNavigate } from "react-router-dom";
 import { ID } from "appwrite";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const AuthContext = createContext();
 
@@ -9,6 +11,19 @@ const AuthContextProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
   const [user, setUser] = useState(null);
   const navigate = useNavigate();
+
+  const notifyLogin = () => {
+    toast.success("logged in!", {
+      position: "top-center",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+  };
 
   useEffect(() => {
     checkUserStatus();
@@ -25,6 +40,7 @@ const AuthContextProvider = ({ children }) => {
       setUser(accountDetails);
 
       navigate("/");
+      notifyLogin();
     } catch (err) {
       console.error(err);
     }
@@ -74,6 +90,18 @@ const AuthContextProvider = ({ children }) => {
   return (
     <AuthContext.Provider value={contextValue}>
       {loading ? <p>loading...</p> : children}
+      <ToastContainer
+        position="top-center"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </AuthContext.Provider>
   );
 };
